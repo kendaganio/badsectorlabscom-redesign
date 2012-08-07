758960c2e874844cd193c970110fce389a5c0027
Ken-Lauren Daganio <akosiken@gmail.com>
2012-08-07 19:33:03 +0800

    finishes styling the header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 /* Author:
-
+  Ken-Lauren Daganio
 */
+$(document).ready(function(){
 
+  $("#headshots img").hover(function(){
+    var pos = $(this).position();
+    var popover = $("#popover");
+    popover.css({
+      left: pos.left - 120,
+      top: pos.top - 120
+    });
+    popover.fadeIn('slow')
+  },function(){
+    $("#popover").hide();
+  });
+
+});
 
 
 
