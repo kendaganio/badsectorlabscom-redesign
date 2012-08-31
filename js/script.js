21079f7a47cafda41c7a398c4734fd0419e9f3b8
Ken-Lauren Daganio <akosiken@gmail.com>
2012-08-31 13:24:31 +0800

    adds js script for fixed navigation

--- js/script.js
+++ js/script.js
@@ -3,16 +3,33 @@
 */
 $(document).ready(function(){
 
-  $("#headshots img").each(function(){
-    var user = $(this).data('twitter-handle');
-    $("#" + user + "-tweet").tweet({
-      join_text: "auto",
-      username: user,
-      count: 1,
-      loading_text: "loading tweets...",
-      refresh_interval: 300
+  var msie6 = $.browser == 'msie' && $.browser.version < 7;
+  if (!msie6) {
+    var top = $('#navigation ul').offset().top - parseFloat($('#navigation').css('margin-top').replace(/auto/, 0));
+    $(window).scroll(function(event) {
+      var y = $(this).scrollTop() - 75;
+      console.log('top:' + top + '   y:' + y)
+      if (y >= top) {
+        $('#navigation').addClass('fixed');
+      }else {
+        $('#navigation').removeClass('fixed');
+      }
     });
-  })
+  }
+
+  $(".page").each(function(){
+    $(this).css("min-height", window.innerHeight);
+  });
+  // $("#headshots img").each(function(){
+  //   var user = $(this).data('twitter-handle');
+  //   $("#" + user + "-tweet").tweet({
+  //     join_text: "auto",
+  //     username: user,
+  //     count: 1,
+  //     loading_text: "loading tweets...",
+  //     refresh_interval: 300
+  //   });
+  // })
 
   $("#headshots img").hover(function(){
     var pos = $(this).position();
