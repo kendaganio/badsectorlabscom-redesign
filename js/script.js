7a219dd9852f1fdf9342d74a057597572b6473ce
Ken-Lauren Daganio <akosiken@gmail.com>
2012-09-01 21:26:32 +0800

    adds scrolling effect in navigation links

--- js/script.js
+++ js/script.js
@@ -8,8 +8,7 @@ $(document).ready(function(){
   if (!msie6) {
     var top = $('#navigation ul').offset().top - parseFloat($('#navigation').css('margin-top').replace(/auto/, 0));
     $(window).scroll(function(event) {
-      var y = $(this).scrollTop() - 75;
-      console.log('top:' + top + '   y:' + y)
+      var y = $(this).scrollTop() - 60;
       if (y >= top) {
         $('#navigation').addClass('fixed');
       }else {
@@ -21,6 +20,12 @@ $(document).ready(function(){
   $(".page").each(function(){
     $(this).css("min-height", window.innerHeight);
   });
+
+  $("#navigation a").click(function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $("" + $(this).attr('href')).offset().top}, 1000);
+  });
+  
   // $("#headshots img").each(function(){
   //   var user = $(this).data('twitter-handle');
   //   $("#" + user + "-tweet").tweet({
